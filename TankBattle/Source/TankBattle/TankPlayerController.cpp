9ec3670116c03027e040251d4ae4d5c2a1af79b4
Alex <lepsalex@gmail.com>
2018-11-14 20:26:20 -0500

    TankPlayerController successfully possessing Tank

--- TankBattle/Source/TankBattle/TankPlayerController.cpp
+++ TankBattle/Source/TankBattle/TankPlayerController.cpp
@@ -2,6 +2,18 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+
+	auto ControllerdTank = GetControlledTank();
+
+	if (!ControllerdTank) {
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController not possessing a tabk"));
+	} else {
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController begin play with controlled tank: %s"), *ControllerdTank->GetName());
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const {
 	return Cast<ATank>(GetPawn());
 }
