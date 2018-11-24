82996715431bead20e57fd27b040a56cf25868bf
Alex <lepsalex@gmail.com>
2018-11-23 21:41:31 -0500

    Aiming Component refactor stage one

--- TankBattle/Source/TankBattle/Tank.cpp
+++ TankBattle/Source/TankBattle/Tank.cpp
@@ -10,23 +10,18 @@
 ATank::ATank() {
     // Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
     PrimaryActorTick.bCanEverTick = false;
-
-    // No need to protect pointers at construction
-    TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
-}
-
-// Called to bind functionality to input
-void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
-    Super::SetupPlayerInputComponent(PlayerInputComponent);
 }
 
 void ATank::AimAt(FVector HitLocation) const {
-    TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
+    if (TankAimingComponent) {
+        TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
+    }
 }
 
 void ATank::Fire() {
     bool isReloaded = (FPlatformTime::Seconds() - LastFireTime) > ReloadTimeInSeconds;
-    if (Barrel && isReloaded) {
+    if (TankAimingComponent && isReloaded) {
+        UTankBarrel* Barrel = TankAimingComponent->GetBarrel();
         AProjectile* Projectile = GetWorld()->SpawnActor<AProjectile>(
                                       ProjectileBlueprint,
                                       Barrel->GetSocketLocation(FName("Projectile")),
@@ -39,11 +34,6 @@ void ATank::Fire() {
     }
 }
 
-void ATank::SetBarrelReference(UTankBarrel* BarrelToSet) {
-    TankAimingComponent->SetBarrelReference(BarrelToSet);
-    Barrel = BarrelToSet;
-}
-
-void ATank::SetTurretReference(UTankTurret* TurretToSet) {
-    TankAimingComponent->SetTurretReference(TurretToSet);
+void ATank::SetAimingComponent(UTankAimingComponent* TankAimingComponentToSet) {
+    TankAimingComponent = TankAimingComponentToSet;
 }
--- TankBattle/Source/TankBattle/Tank.h
+++ TankBattle/Source/TankBattle/Tank.h
@@ -16,27 +16,14 @@ class TANKBATTLE_API ATank : public APawn {
     GENERATED_BODY()
 
   public:
-    UFUNCTION(BlueprintCallable)
-    void SetBarrelReference(UTankBarrel* BarrelToSet);
-
-    UFUNCTION(BlueprintCallable)
-    void SetTurretReference(UTankTurret* TurretToSet);
-
     void AimAt(FVector HitLocation) const;
 
     UFUNCTION(BlueprintCallable)
     void Fire();
 
-  protected:
-    UPROPERTY(BlueprintReadOnly)
-    UTankAimingComponent* TankAimingComponent = nullptr;
-
   private:
     // Sets default values for this pawn's properties
     ATank();
-
-    // Called to bind functionality to input
-    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
     UPROPERTY(EditDefaultsOnly, Category = "Setup")
     TSubclassOf<AProjectile> ProjectileBlueprint;
@@ -46,8 +33,6 @@ class TANKBATTLE_API ATank : public APawn {
 
     UPROPERTY(EditDefaultsOnly, Category = "Firing")
     float ReloadTimeInSeconds = 3;
-
-    UTankBarrel* Barrel = nullptr;
 
     double LastFireTime = 0;
 };
